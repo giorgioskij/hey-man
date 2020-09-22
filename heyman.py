ccbfd63a8c23b6aa9cff9d489ccb07bcd3bd3c9a
Giorgio Strano <giorgiopuntostrano@gmail.com>
2020-09-23 00:44:34 +0200

    basic functions included in a class "Heyman"

---
+++ heyman.py
@@ -0,0 +1,98 @@
+import signal
+import sys
+from snowboylib import snowboydecoder
+
+
+class Heyman:
+
+    def __init__(self, model):
+        self.model = model
+        self.interrupted = False
+        self.detector = snowboydecoder.HotwordDetector(self.model, sensitivity = 0.5)
+
+        signal.signal(signal.SIGINT, self.handle_signal)
+
+    def listen(self):
+        print("Listening... press Ctrl+C to exit")
+        self.detector.start(detected_callback = self.activate,
+                            interrupt_check = lambda: self.interrupted,
+                            sleep_time = 0.03)
+
+        self.detector.terminate()
+
+    def handle_signal(self):
+        self.interrupted = True
+
+    def say(self, s):
+        os.system("say {0} -v fiona -r 210".format(s.replace("\'", "")))
+
+    def activate(self):
+        print("Hi! I am now listening")
+        self.say("what's up man")
+
+        self.detector.terminate()
+        mic = sr.Microphone()
+        rec = sr.Recognizer()
+
+        with mic as source:
+            audio = rec.listen(source=source)
+
+        try:
+            text = rec.recognize_google(audio)
+
+        except sr.UnknownValueError:
+            print("Google could not understand audio")
+
+        except sr.RequestError as e:
+            print("Could not request results from shpinx service; {0}".format(e))
+
+        if text:
+            self.interpret(text)
+
+        self.listen()
+
+
+    def interpret(self, text):
+        if "open" in text or "launch" in text:
+            words = text.split()
+            i = words.index("open") if "open" in text else words.index("launch")
+            openApp("".join(words[i+1:]))
+
+        elif "nothing" in text:
+            self.say("ok sorry")
+
+        else:
+            answer = "You said {0}, but you know I am not yet programmed to answer to that".format(text.replace("\'", ""))
+            # os.system("say {0}".format(answer))
+            self.say(answer)
+
+
+
+def openApp(name):
+    newname = ''.join(' ' + char if char.isupper() else char for char in name).strip() + ".app"
+
+    print("I heard {0}".format(newname))
+
+    best_score = inf
+    candidate = None
+    for program in programs:
+        score = Levenshtein.distance(newname, program)
+        if score < best_score:
+            best_score = score
+            candidate = program
+
+    path = candidate.replace(" ", "\\ ")
+
+    say("launching {0}".format(candidate))
+
+    os.system("open /Applications/"+path)
+
+
+
+if len(sys.argv) > 1:
+    model = sys.argv[1]
+else:
+    model = "heyman.pmdl"
+
+heyman = Heyman(model)
+heyman.listen()
